37acb531d2e7a536e83ce7b43f85d975ff676c1b
Brian Thompson <bthompson@mindgrub.com>
2019-01-15 10:19:31 -0500

    Merge latest changed and let tests run.

--- .circleci/test-example-repo.sh
+++ .circleci/test-example-repo.sh
@@ -36,17 +36,22 @@ then
     BUILD_TOOLS_VERSION="${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
 # Otherwise use the current branch
 else
-    BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+    # If on root repo use the current branch
+    if [[ $CIRCLE_PROJECT_USERNAME == "pantheon-systems" ]]; then
+        BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+    # Otherwise use the dev tip from the pantheon-systems repo
+    else
+        BUILD_TOOLS_VERSION="dev-master"
+    fi
 fi
 
 if [ "$GIT_PROVIDER" == "github" ]; then
-    TARGET_REPO=$GITHUB_USERNAME/$TERMINUS_SITE
+    TARGET_REPO=$GITHUB_USER/$TERMINUS_SITE
     CLONE_URL="https://github.com/${TARGET_REPO}.git"
 else
     if [ "$GIT_PROVIDER" == "bitbucket" ]; then
-        TARGET_REPO=$BITBUCKET_USERNAME/$TERMINUS_SITE
-        # Bitbucket repo is private, thus HTTP basic auth is integrated into clone URL
-        CLONE_URL="https://$BITBUCKET_USER:$BITBUCKET_PASS@bitbucket.org/${TARGET_REPO}.git"
+        TARGET_REPO=$BITBUCKET_USER/$TERMINUS_SITE
+        CLONE_URL="https://$BITBUCKET_USER@bitbucket.org/${TARGET_REPO}.git"
     else
         echo "Unsupported GIT_PROVIDER. Valid values are: github, bitbucket"
         exit 1
@@ -56,7 +61,7 @@ fi
 terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=$GIT_PROVIDER --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
 # Confirm that the Pantheon site was created
 terminus site:info "$TERMINUS_SITE"
-# Confirm that the Github project was created
+# Confirm that the Github or Bitbucket project was created
 git clone "$CLONE_URL" "$TARGET_REPO_WORKING_COPY"
 # Confirm that Circle was configured for testing, and that the first test passed.
 
